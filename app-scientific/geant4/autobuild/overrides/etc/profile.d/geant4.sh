48fc0090ed91a1553163f63a5beea138dc1d8786
CAB233 <yidaduizuoye@outlook.com>
2024-12-09 22:01:56 +0800

    geant4: update to 11.3.0
    - Resolve OpenGL compatibility issue with some GPUs
    - Move to app-scientific
    - Use anitya to check for updates

---
+++ app-scientific/geant4/autobuild/overrides/etc/profile.d/geant4.sh
@@ -0,0 +1,3 @@
+# Resolve OpenGL compatibility issues with some GPUs by using a specified graphics system
+# Ref: https://geant4-userdoc.web.cern.ch/UsersGuides/AllGuides/html/ForApplicationDevelopers/Visualization/visdrivers.html
+export G4VIS_DEFAULT_DRIVER=TSG
