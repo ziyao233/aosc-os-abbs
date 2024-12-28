c64f29443a392e472a98472595ee250c811161cb
xtex <xtexchooser@duck.com>
2024-12-28 18:04:07 +0800

    openjfx: replace _JAVA_OPTIONS with JDK_JAVA_OPTIONS

--- lang-java/openjfx-common/autobuild/overrides/etc/profile.d/openjfx.sh
+++ lang-java/openjfx-common/autobuild/overrides/etc/profile.d/openjfx.sh
@@ -1,1 +1,3 @@
-export _JAVA_OPTIONS="$_JAVA_OPTIONS --module-path=/usr/share/openjfx/lib/"
+# use JDK_JAVA_OPTIONS rather than _JAVA_OPTIONS to avoid
+# OpenJDK 8 or earlier picking up unsupported options
+export JDK_JAVA_OPTIONS="$JDK_JAVA_OPTIONS --module-path=/usr/share/openjfx/lib/"
